40ecf411ee137cc58ab4d29db68ecd4b55e3d56a
luanaMQ@outlook.com <luanaMQ@outlook.com>
2022-09-23 16:03:18 -0300

    .h y main 1

---
+++ TP FINAL Grupo 4/Source.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cServidor* _servidor = new cServidor();  //inicializamos el servidor
+	//Le agregamos los usuarios
+	cUsuarios* usuario1 = new cUsuarios();  //Agregar _
+	cServicios* servicio1 = new cVideos();
+	cFecha* FechaHoy = new cFecha();
+
+	_servidor->AgregarUsuario (usuario1); //Agregamos el usuario
+
+	_servidor->AgregarServicio(servicio1); //
+
+	for (int i = 0; i < _servidor->listaUsuarios()->getCA(); i++) {
+		cUsuario* usuario1 = listaUsuario->get(i);
+		
+		_servidor->LoggearUsuario(usuario1);
+
+
+		_servidor->ListarServicios(usuario1);  //Depende el pais, enlista los sevicios disponibles por pais
+
+		usuario1->SeleccionarServicio(servicio1);
+
+		//Hacer for
+
+		usuario1->UsarServicio();
+		_servidor->Desconectar(usuario1);
+	}
+
+	  
+
+	_servidor->cBackUp(FechaHoy);  
+	_servidor->GetEstadisticas();  //void que imprime las estadisticas
+
+	//Hacer deletes
+}
---
+++ TP FINAL Grupo 4/cAudios.cpp
@@ -0,0 +1,1 @@
+#include "cAudios.h"
